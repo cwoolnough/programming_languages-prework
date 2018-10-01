a51176309c56fb73e4f61a11dd5811a39c329953
cwoolnoughchamorro <cwoolnoughchamorro@googlemail.com>
2018-10-01 16:59:32 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,7 +4,7 @@ def reformat_languages(languages)
     languages.each do |styles, lang|
       lang.each do |lang, type|
         new_hash[lang] = type
-        
+        new_hash[style]
     end 
   end
   new_hash
